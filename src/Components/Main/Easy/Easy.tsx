836b87919d7d1c77c1f808e19a9e6cee63155d59
itellida <ne8357959@gmail.com>
2023-06-12 00:12:07 +0600

    elli2

--- src/Components/Main/Easy/Easy.tsx
+++ src/Components/Main/Easy/Easy.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import '../Easy/Easy.scss'
-
+import imgEasy from "../../../Imgs/Rectangle 3.svg"
 
 const Easy = () => {
     return (
-        <div>
-        </div>
+        <section id="easy">
+            <div className="container">
+                <div className="easy">
+                    <h1>Easy Home Search</h1>
+                    <p>We can help you to find your dream home</p>
+
+                    <div className="easy--block"><div className="easy--blocks">
+                        <img src={imgEasy} alt=""/>
+                        <div className="easy--box">
+                            <h2>Find your SuperAgent</h2>
+                            <h4 className="easy--desc">The most responsive agents with up-to-date
+                                and improved accuracy on the properties  you are searching for.</h4>
+                        </div>
+                    </div>
+                        <div className="easy--blocks">
+                            <img src={imgEasy} alt=""/>
+                            <div className="easy--box">
+                                <h2>Find your SuperAgent</h2>
+                                <h4 className="easy--desc">The most responsive agents with up-to-date
+                                    and improved accuracy on the properties  you are searching for.</h4>
+                            </div>
+                        </div></div>
+
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/Components/Main/Hero/Hero.tsx
+++ src/Components/Main/Hero/Hero.tsx
@@ -1,12 +1,65 @@
 import React from 'react';
 import '../Hero/Hero.scss'
-
+import imgHero from "../../../Imgs/Image.svg"
+import img1Hero from "../../../Imgs/Image (1).svg"
+import {CiSearch} from "react-icons/ci"
+import Slider from "react-slick"
 
 const Hero = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
     return (
-        <div>
+        <section id="hero">
+            <div className="container">
+                <div className="hero">
+                    <img className="hero--img1" src={imgHero} alt=""/>
+                    <img className="hero--img2" src={img1Hero} alt=""/>
+                    <h1 >Let’s find a perfect <br/> home for you!</h1>
 
-        </div>
+                    <div className="hero--block">
+                        <div>  <div className="hero--buttons">
+                            <button className="hero--btn1">Buy</button>
+                            <button className="hero--btn1">Rent</button>
+                        </div>
+
+                            <div className="hero--buttons">
+                                <select name="" id="" className="hero--options">
+                                    <option value="">Property type</option>
+                                    <option value="">Apartment</option>
+                                    <option value="">Villa</option>
+                                    <option value="">Townhouse</option>
+                                    <option value="">Penthouse</option>
+                                    <option value="">Whole building</option>
+                                </select>
+
+                                <select name="" id="" className="hero--option2">
+                                    <option value="">Rooms</option>
+                                    <option value="">1</option>
+                                    <option value="">2</option>
+                                    <option value="">3</option>
+                                    <option value="">4</option>
+                                    <option value="">5</option>
+                                    <option value="">5+</option>
+                                </select>
+                                <select name="" id="" className="hero--option3">
+                                    <option value="">Price</option>
+                                    <option value="">| min</option>
+                                    <option value="">| max</option>
+                                </select>
+                            </div></div>
+
+                        <input className="hero--input1" type="text" placeholder="Enter an address or city "/>
+                        <button className="hero--btn2"> <CiSearch/> Search</button>
+                    </div>
+                </div>
+
+            </div>
+        </section>
     );
 };
 
