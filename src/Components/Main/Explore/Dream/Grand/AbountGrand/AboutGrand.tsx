6577b9fbe8f98fa21b2b7c9833d305161a291e74
macbook_pro <syrgabek0131@gmail.com>
2023-07-18 11:37:54 +0600

    adaptation grand

--- src/Components/Main/Explore/Dream/Grand/AbountGrand/AboutGrand.tsx
+++ src/Components/Main/Explore/Dream/Grand/AbountGrand/AboutGrand.tsx
@@ -8,15 +8,15 @@ import {HiOutlineBuildingOffice2} from "react-icons/hi2";
 
 const AboutGrand = () => {
     return (
-        <div className="container text-white rounded-2xl bg-blue-500 text-xl 992:text-lg flex items-center justify-around flex-wrap py-3">
-            <div className="flex items-center 1440:mx-[6%] mb-4 1200:mx-[4%] 1024:mx-[3%] 992:mx-[9px]"><BsHouse className="mr-1"/><span>Apartment</span></div>
-            <div className="flex items-center 1440:mx-[6%] mb-4 1200:mx-[4%] 1024:mx-[3%] 992:mx-[9px]"><BiBed className="mr-1"/><span>1 Bedroom</span></div>
-            <div className="flex items-center 1440:mx-[6%] mb-4 1200:mx-[4%] 1024:mx-[3%] 992:mx-[9px]"><BiCabinet className="mr-1"/><span>Furnished</span></div>
-            <div className="flex items-center 1440:mx-[6%] mb-4 1200:mx-[4%] 1024:mx-[3%] 992:mx-[9px]"><AiOutlineUserAdd className="mr-1"/><span>Without settlement</span></div>
-            <div className="flex items-center 1440:mx-[6%] 1200:mx-[4%] 1024:mx-[3%] 992:mx-[9px]"><BiSelection className="mr-1"/><span>Square: 120 m2</span></div>
-            <div className="flex items-center 1440:mx-[6%] 1200:mx-[4%] 1024:mx-[3%] 992:mx-[9px]"><MdOutlineBathtub className="mr-1"/><span>1 Bathroom</span></div>
-            <div className="flex items-center 1440:mx-[6%] 1200:mx-[4%] 1024:mx-[3%] 992:mx-[9px]"><LuBuilding className="mr-1"/><span>Floor:3 of 12</span></div>
-            <div className="flex items-center 1440:mx-[6%] 1200:mx-[4%] 1024:mx-[3%] 992:mx-[9px]"><HiOutlineBuildingOffice2 className="mr-1"/><span>Serie: Elite</span></div>
+        <div className="container max-768:!container text-white rounded-2xl bg-blue-500 text-xl 1024:text-lg 992:text-lg  flex items-center justify-around flex-wrap py-3 max-768:rounded-[0] ">
+            <div className="flex items-center 1440:mx-[6%] mb-4 1200:mx-[4%] 1024:mx-[3%] 992:mx-[9px] 768:mx-[66px]"><BsHouse className="mr-1"/><span>Apartment</span></div>
+            <div className="flex items-center 1440:mx-[6%] mb-4 1200:mx-[4%] 1024:mx-[3%] 992:mx-[9px] 768:mx-[66px]"><BiBed className="mr-1"/><span>1 Bedroom</span></div>
+            <div className="flex items-center 1440:mx-[6%] mb-4 1200:mx-[4%] 1024:mx-[3%] 992:mx-[9px] 768:mx-[66px]"><BiCabinet className="mr-1"/><span>Furnished</span></div>
+            <div className="flex items-center 1440:mx-[6%] mb-4 1200:mx-[4%] 1024:mx-[3%] 992:mx-[9px] 768:mx-[66px]"><AiOutlineUserAdd className="mr-1"/><span>Without settlement</span></div>
+            <div className="flex items-center 1440:mx-[6%] 1200:mx-[4%] 1024:mx-[3%] 992:mx-[9px] 768:mx-[66px] 768:mb-4"><BiSelection className="mr-1"/><span>Square: 120 m2</span></div>
+            <div className="flex items-center 1440:mx-[6%] 1200:mx-[4%] 1024:mx-[3%] 992:mx-[9px] 768:mx-[66px] 768:mb-4"><MdOutlineBathtub className="mr-1"/><span>1 Bathroom</span></div>
+            <div className="flex items-center 1440:mx-[6%] 1200:mx-[4%] 1024:mx-[3%] 992:mx-[9px] 768:mx-[66px] 768:mb-4"><LuBuilding className="mr-1"/><span>Floor:3 of 12</span></div>
+            <div className="flex items-center 1440:mx-[6%] 1200:mx-[4%] 1024:mx-[3%] 992:mx-[9px] 768:mx-[66px] 768:mb-4"><HiOutlineBuildingOffice2 className="mr-1"/><span>Serie: Elite</span></div>
         </div>
     );
 };
--- src/Components/Main/Explore/Dream/Grand/GrandAmenities/GrandAmenities.tsx
+++ src/Components/Main/Explore/Dream/Grand/GrandAmenities/GrandAmenities.tsx
@@ -8,11 +8,11 @@ import Agent from "../../../../../../Imgs/grant-agent.svg"
 
 const GrandAmenities = () => {
     return (
-        <div className="container">
+        <div className="container mb-8">
             <div className="flex items-start justify-between">
                 <div>
                     <h2 className="text-2xl font-semibold mb-4 mt-8">Amenities</h2>
-                    <div className="text-xl w-[70%] 1200:w-[90%] flex items-center flex-wrap ">
+                    <div className="text-xl w-[80%] 1200:text-lg 1200:w-[90%] 1024:text-base 1024:w-[90%] flex 992:w-[52%] items-center flex-wrap ">
                         <div className="flex items-center mr-16 my-3"><BsDoorClosed className="mr-[10px]"/><span>Balkony</span></div>
                         <div className="flex items-center mr-16 my-3"><LuParkingSquare className="mr-[10px]"/><span>Covered parking </span></div>
                         <div className="flex items-center mr-16 my-3"><GiWashingMachine className="mr-[10px]"/><span>Washing machine</span></div>
@@ -23,16 +23,16 @@ const GrandAmenities = () => {
                         <div className="flex items-center mr-16 my-3"><MdComputer className="mr-[10px]"/><span>TV</span></div>
                     </div>
                 </div>
-                <div className="w-[34%] flex flex-col items-center mt-10">
-                    <h3 className="text-xl font-semibold mb-3">Owner</h3>
+                <div className="w-[46%] 1024:w-[50%] 992:w-[280px] flex flex-col items-center mt-10">
+                    <h3 className="text-xl 1024:text-lg font-semibold mb-3">Owner</h3>
                     <img src={Agent} alt=""/>
-                    <h4 className="border-b-2 text-blue-500 border-b-blue-500 text-xl my-3">Timur Timurov</h4>
-                    <button className="border-2 rounded-xl  border-gray-200 py-2 px-16 my-2 text-xl 1200:text-lg 1200:px-4">Contact owner</button>
+                    <h4 className="border-b-2 text-blue-500 border-b-blue-500 text-xl 1024:text-lg my-3">Timur Timurov</h4>
+                    <button className="border-2 rounded-xl  border-gray-200 py-2 px-16 1024:px-10 my-2 text-xl 1200:text-lg 1024:text-base 992:text-lg 1200:px-4">Contact owner</button>
                 </div>
             </div>
             <div>
                 <h2 className="text-2xl font-semibold mb-4 mt-8">House roules</h2>
-                <div className="text-xl flex items-center flex-wrap ">
+                <div className="text-xl 1024:text-lg flex items-center flex-wrap ">
                     <div className="flex items-center mr-16 my-3"><MdSmokeFree className="mr-[10px]"/><span>Smoking in the  apartment</span></div>
                     <div className="flex items-center mr-16 my-3"><BsVolumeUp className="mr-[10px]"/><span>Listen to music loudly</span></div>
                 </div>
